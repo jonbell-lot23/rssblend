79c7909eac72ac53f7cfc0bab0cb9fc964381d30
Jon Bell <github@lot23.com>
2023-01-09 20:54:04 +1300

    Initial

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "md", "mdx"],
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    // ...
+  ],
+};
