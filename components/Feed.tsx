f310de1e30beaf0419260a68d6b57399c5a8b73a
Jon Bell <github@lot23.com>
2023-01-15 07:57:33 +0000

    Some more color tweaks

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -79,7 +79,7 @@ const Feed = () => {
                     <div>
                       <a
                         href={item.link}
-                        className="text-pink-600 text-decoration-none text-truncate break-normal text-xl"
+                        className="text-[#E9496F] text-decoration-none text-truncate break-normal text-xl"
                       >
                         {item.title.replace(/<[^>]+>/g, "").trim() !==
                         item.description.replace(/<[^>]+>/g, "").trim()
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="bg-pink-600 text-white top-0 left-0 w-full z-10">
+      <header className="bg-[#E9496F] text-white top-0 left-0 w-full z-10">
         <div className="container mx-auto px-6 py-3 shadow-b-md">
           <h1 className="text-xl font-medium">Jon Bell's Firehose</h1>
         </div>
