215bb811c6fa00fe16fe5d9dffcb3945cf6afc40
Jon Bell <github@lot23.com>
2023-03-11 22:27:01 +0000

    Little demo

--- pages/db-test.tsx
+++ pages/db-test.tsx
@@ -1,35 +1,59 @@
 import Head from "next/head";
 import React from "react";
 import { GetStaticProps } from "next";
+import Link from "next/link";
+import styles from "styles/bouquet.module.css";
 
-import { PrismaClient, Post } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-export const getStaticProps: GetStaticProps = async () => {
-  console.log("Fetching posts!");
-  const posts = await prisma.post.findMany();
-  console.log("Posts:", posts);
+export const getStaticProps = async () => {
+  try {
+    const posts = await prisma.$queryRaw`
+      SELECT b.id, b.date, b.name, b.detail, b.url, b.username, lookup.emoji
+      FROM bouquet b
+      LEFT JOIN bouquet_emoji_lookup lookup ON b.name = lookup.name
+      ORDER BY b.date ASC limit 4
+    ;`;
 
-  return {
-    props: {
-      posts,
-    },
-    revalidate: 10,
-  };
+    return {
+      props: {
+        posts,
+      },
+      revalidate: 1201,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        posts: [],
+      },
+      revalidate: 1200,
+    };
+  }
 };
 
-const Home = ({ posts }: { posts: Post[] }) => {
-  console.log("Posts prop:", posts);
-
+const Home = ({ posts }) => {
   return (
-    <div>
-      <Head>
-        <title>My Blog</title>
-      </Head>
+    <div className={styles.container}>
       {posts.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
+        <div key={post.id} className={styles.post}>
+          <span className={styles.emoji}>
+            <Link href={`/category/${post.name}`}>
+              {post.emoji ? post.emoji : post.name}
+            </Link>
+          </span>
+          <div className={styles.details}>
+            <span className={styles.detail}>{post.detail}</span>
+
+            <div className={styles.metadata}>
+              <span className={styles.date}>{post.date}</span>
+              <span className={styles.username}>
+                <Link href={`/user/${post.username}`}>{post.username}</Link>
+              </span>
+            </div>
+          </div>
         </div>
       ))}
     </div>
