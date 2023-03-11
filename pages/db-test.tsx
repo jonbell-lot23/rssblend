1f7d4c2beb0610675c3ad4bbf92d258f05c11f59
Jon Bell <github@lot23.com>
2023-03-11 21:57:04 +0000

    DB stuff

---
+++ pages/db-test.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import React from "react";
+import { GetStaticProps } from "next";
+
+import { PrismaClient, Post } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log("Fetching posts!");
+  const posts = await prisma.post.findMany();
+  console.log("Posts:", posts);
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 10,
+  };
+};
+
+const Home = ({ posts }: { posts: Post[] }) => {
+  console.log("Posts prop:", posts);
+
+  return (
+    <div>
+      <Head>
+        <title>My Blog</title>
+      </Head>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
