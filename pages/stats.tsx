f8cc5e2eb4c640d92e1db8b192feb5737817b7bc
Jon Bell <github@lot23.com>
2024-03-22 20:08:37 +1300

    Title

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -17,9 +17,7 @@ const Home = () => {
 
       <header className="bg-[#E9496F] text-white top-0 left-0 w-full z-10">
         <div className="container px-6 py-3 mx-auto shadow-b-md">
-          <h1 className="text-xl font-medium text-center">
-            Jon Bell's Firehose
-          </h1>
+          <h1 className="text-xl font-medium text-center">Firehose</h1>
         </div>
       </header>
       <div className="max-w-screen-lg mx-auto">
