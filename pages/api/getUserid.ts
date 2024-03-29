0beb122be4e024ad51f8f7892047ab0fbcd88d6c
Jon Bell <github@lot23.com>
2024-03-29 14:52:49 +1300

    Fixing?

--- pages/api/getUserid.ts
+++ pages/api/getUserid.ts
@@ -5,9 +5,8 @@ const prisma = new PrismaClient();
 
 const getUser = async (req: NextApiRequest, res: NextApiResponse) => {
     const { username } = req.query;
-    const user = await prisma.User.findFirst({ where: { name: username } });
+    const user = await prisma.user.findFirst({ where: { name: username } });
     res.json({ userid: user?.id });
   };
   
   export default getUser;
-  
